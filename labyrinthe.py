fc292b00a4c96cef8288a10852486e263abe2728
Louis Allard <la.devweb@yahoo.com>
2021-03-26 10:48:05 +0100

    Fix.

--- labyrinthe.py
+++ labyrinthe.py
@@ -7,8 +7,6 @@ GREEN = (0,255,0)
 RED = (255,0,0)
 PURPLE = (255,0,255)
 PERSO = (255,215,0)
-player_x = 67
-player_y = 92
 
 class Wall(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, colour):
@@ -19,64 +17,29 @@ class Wall(pygame.sprite.Sprite):
         self.rect.y = y
         self.rect.x = x
 
-# Player class
-# changespeed to change the player speed while it's progress
-# move to move the player
-
-class Player2(pygame.sprite.Sprite):
-    move_ppx = 0
-    move_ppy = 0
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pp_x,pp_y):
+        super().__init__()
+        move_ppx = 0
+        move_ppy = 0
     def changespeed(self,pp_x,pp_y):
         self.move_ppx += pp_x
-        self.move_ppy += pp_y
+        self.move_ppy += pp_y 
     def move(self, walls):
-        self.rect.pp_x += self.move_ppx
+        self.pp_x += self.move_ppx
         block_hit_list = pygame.sprite.spritecollide(self, walls, False)
         for block in block_hit_list:
             if self.move_ppx > 0:
-                self.rect.right = block.rect.left
+                self.right = block.left
             else:
-                self.rect.left = block.rect.right
-        self.rect.pp_y += self.move_ppx
+                self.left = block.right
+        self.pp_y += self.move_ppx
         block_hit_list = pygame.sprite.spritecollide(self, walls, False)
         for block in block_hit_list:
             if self.move__ppy > 0:
-                self.rect.bottom = block.rect.top
+                self.bottom = block.top
             else:
-                self.rect.top = block.rect.bottom        
-
-
-""" class Player(pygame.sprite.Sprite):
-    change_x = 0
-    change_y = 0
-# Create the rectangle
-
-    def __init__(self, x, y):
-        super().__init__()
-        self.image = pygame.Surface([15, 15])
-        self.image.fill(PERSO)
-        self.rect = self.image.get_rect()
-        self.rect.y = y
-        self.rect.x = x
-    def changespeed(self, x, y):
-        self.change_x += x
-        self.change_y += y
-    def move(self, walls):
-        self.rect.x += self.change_x
-        block_hit_list = pygame.sprite.spritecollide(self, walls, False)
-        for block in block_hit_list:
-            if self.change_x > 0:
-                self.rect.right = block.rect.left
-            else:
-                self.rect.left = block.rect.right
-        self.rect.y += self.change_y
-        block_hit_list = pygame.sprite.spritecollide(self, walls, False)
-        for block in block_hit_list:
-            if self.change_y > 0:
-                self.rect.bottom = block.rect.top
-            else:
-                self.rect.top = block.rect.bottom """
-
+                self.top = block.bottom        
 # Parent class
 
 class Room(object):
@@ -102,23 +65,6 @@ class Room1(Room):
             wall = Wall(item[0], item[1], item[2], item[3], item[4])
             self.wall_list.add(wall)
 
-class Room2(Room):
-    def __init__(self):
-        super().__init__()
-        walls = [[0, 0, 20, 250, RED],
-                 [0, 350, 20, 250, RED],
-                 [780, 0, 20, 250, RED],
-                 [780, 350, 20, 250, RED],
-                 [20, 0, 760, 20, RED],
-                 [20, 580, 760, 20, RED],
-                 [190, 30, 20, 500, GREEN],
-                 [590, 50, 20, 520, GREEN]
-                 ]
-        for item in walls:
-            wall = Wall(item[0], item[1], item[2], item[3], item[4])
-            self.wall_list.add(wall)
-
-class Room3(Room):
     def __init__(self):
         super().__init__()
         walls = [[0, 0, 20, 250, PURPLE],
@@ -156,18 +102,20 @@ def main():
     pp_x = 150
     pp_y = 200
     img = pygame.image.load("./sprites/sprite.png")
-    player2 = Player2(pp_x,pp_y,img,screen)
+    player = Player(pp_x,pp_y) #attributes are about first sprite position
     screen.blit(img,(pp_x,pp_y))
 ##    
     movingsprites = pygame.sprite.Group()
-    movingsprites.add(player2)
+    movingsprites.add(player)
     rooms = []
     room = Room1()
     rooms.append(room)
+    '''
     room = Room2()
     rooms.append(room)
     room = Room3()
     rooms.append(room)
+    '''
     current_room_no = 0
     current_room = rooms[current_room_no]
     clock = pygame.time.Clock()
@@ -179,57 +127,56 @@ def main():
                 done = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    player2.changespeed(-5, 0)
+                    player.changespeed(-5, 0)
                 if event.key == pygame.K_RIGHT:
-                    player2.changespeed(5, 0)
+                    player.changespeed(5, 0)
                 if event.key == pygame.K_UP:
-                    player2.changespeed(0, -5)
+                    player.changespeed(0, -5)
                 if event.key == pygame.K_DOWN:
-                    player2.changespeed(0, 5)
+                    player.changespeed(0, 5)
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
-                    player2.changespeed(5, 0)
+                    player.changespeed(5, 0)
                 if event.key == pygame.K_RIGHT:
-                    player2.changespeed(-5, 0)
+                    player.changespeed(-5, 0)
                 if event.key == pygame.K_UP:
-                    player2.changespeed(0, 5)
+                    player.changespeed(0, 5)
                 if event.key == pygame.K_DOWN:
-                    player2.changespeed(0, -5)
+                    player.changespeed(0, -5)
 #walls and pieces
-        player2.move(current_room.wall_list)
+        player.move(current_room.wall_list)
 
-        if player2.rect.ppx < -15:
+        if player.rect.ppx < -15:
             if current_room_no == 0:
                 current_room_no = 2
                 current_room = rooms[current_room_no]
-                player2.rect.ppx = 790
+                player.rect.ppx = 790
             elif current_room_no == 2:
                 current_room_no = 1
                 current_room = rooms[current_room_no]
-                player2.rect.ppx = 790
+                player.rect.ppx = 790
             else:
                 current_room_no = 0
                 current_room = rooms[current_room_no]
-                player2.rect.ppx = 790
+                player.rect.ppx = 790
 
-        if player2.rect.ppx > 801:
+        if player.rect.ppx > 801:
             if current_room_no == 0:
                 current_room_no = 1
                 current_room = rooms[current_room_no]
-                player2.rect.ppx = 0
+                player.rect.ppx = 0
             elif current_room_no == 1:
                 current_room_no = 2
                 current_room = rooms[current_room_no]
-                player2.rect.ppx = 0
+                player.rect.ppx = 0
             else:
                 current_room_no = 0
                 current_room = rooms[current_room_no]
-                player2.rect.ppx = 0
+                player.rect.ppx = 0
     # renonciation clause
         rect = bg.get_rect()
         screen.fill(BLACK)
         screen.blit(bg, rect)
-        Player2(pp_x,pp_y,img,screen)
         pygame.draw.rect(screen, RED, rect, 1)
         movingsprites.draw(screen)
         current_room.wall_list.draw(screen)
